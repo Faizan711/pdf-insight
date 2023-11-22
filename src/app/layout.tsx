96a0a6a39108362e9fbb32a1de39651eeb75cad2
Faizan <faizanalam980@gmail.com>
2023-11-22 20:31:58 +0530

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GeistSans } from "geist/font/sans"; 
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='light'>
+      <body className={cn("min-h-screen font-sans antialiased grainy", GeistSans.className)}>{children}</body>
     </html>
   )
 }
