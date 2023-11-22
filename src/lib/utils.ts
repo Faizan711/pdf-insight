f7487ab6ee0d7aaf3c4d5ef40adb4b2673571ab5
Faizan <faizanalam980@gmail.com>
2023-11-22 20:30:53 +0530

    function to merge css

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import clsx, { ClassValue } from 'clsx';
-import {twMerge} from 'tailwind-merge';
-
-export function cn(...inputs : ClassValue[]){
-    return twMerge(clsx(inputs));
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
