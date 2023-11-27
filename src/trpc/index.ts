c6cd762d285c311e8df83be19c5160e62130ab1f
Faizan <faizanalam980@gmail.com>
2023-11-27 19:07:41 +0530

    set up for trpc

---
+++ src/trpc/index.ts
@@ -0,0 +1,36 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () => {
+    //check user in Kinde
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user || !user.email) 
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    //check user in DB otherwise put in DB
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    })
+
+    if(!dbUser){
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      })
+    }
+
+    return { success: true };
+  }),
+});
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
