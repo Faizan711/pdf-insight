4446dcd8fc868eed1f21cff0d4d57369c58c536d
Faizan <faizanalam980@gmail.com>
2023-11-29 21:04:22 +0530

    added api for getting all files of user

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 
@@ -30,6 +30,16 @@ export const appRouter = router({
 
     return { success: true };
   }),
+  getUserFiles: privateProcedure.query(async ({ctx}) => {
+    const { userId } = ctx
+    
+
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    })
+  })
 });
 // Export type router type signature,
 // NOT the router itself.
