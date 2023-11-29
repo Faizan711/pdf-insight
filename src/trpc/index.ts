9d8dcc611dfc1480fc35b8861edae724e8d300e7
Faizan <faizanalam980@gmail.com>
2023-11-29 21:50:50 +0530

    added api route to delete files

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from "zod";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -9,37 +10,57 @@ export const appRouter = router({
     const { getUser } = getKindeServerSession();
     const user = await getUser();
 
-    if (!user || !user.email) 
-      throw new TRPCError({ code: "UNAUTHORIZED" });
+    if (!user || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
     //check user in DB otherwise put in DB
     const dbUser = await db.user.findFirst({
       where: {
-        id: user.id
-      }
-    })
+        id: user.id,
+      },
+    });
 
-    if(!dbUser){
+    if (!dbUser) {
       await db.user.create({
         data: {
           id: user.id,
-          email: user.email
-        }
-      })
+          email: user.email,
+        },
+      });
     }
 
     return { success: true };
   }),
-  getUserFiles: privateProcedure.query(async ({ctx}) => {
-    const { userId } = ctx
-    
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
 
     return await db.file.findMany({
       where: {
-        userId
-      }
-    })
-  })
+        userId,
+      },
+    });
+  }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
+    }),
 });
 // Export type router type signature,
 // NOT the router itself.
