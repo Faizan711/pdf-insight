c3d4e1819763abc736d359c789d00b254b1cd4ad
Faizan <faizanalam980@gmail.com>
2023-12-01 19:23:26 +0530

    uploadthing setup for pdf upload

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,15 +1,33 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
  
 const f = createUploadthing();
  
 export const ourFileRouter = {
  
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
    
     .middleware(async ({ req }) => {
-      return { };
+
+      const { getUser } = getKindeServerSession();
+      const user = await getUser();
+
+      if(!user || !user.id) throw new Error("unauthorized")
+
+      return {userId: user.id };
     })
-    .onUploadComplete(async ({ metadata, file }) => {}),
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: "PROCESSING",
+        }
+      })
+    }),
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
