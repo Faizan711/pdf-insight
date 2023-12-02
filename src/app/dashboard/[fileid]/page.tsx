01765f77dcd95cc17df507c4e29e46e04747759a
Faizan <faizanalam980@gmail.com>
2023-12-02 22:27:18 +0530

    added prop to pdfrenderer component

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,54 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import PdfRenderer from "@/components/PdfRenderer";
+import ChatWrapper from "@/components/ChatWrapper";
+
+interface PageProps{
+    params: {
+        fileid: string
+    }
+}
+
+const Page = async ({params}: PageProps) => {
+    //get fileid in URL from params
+    const {fileid} = params
+
+    //check user logged in
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if(!user || !user.id){
+        redirect(`/auth-callback?origin=dashboard/${fileid}`)
+    }
+
+    //check in DB for file
+    const file = await db.file.findFirst({
+        where:{
+        id: fileid,
+        userId: user.id,
+        },
+    })
+
+    if(!file) notFound()
+    
+    return (
+        <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+            <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2 ">
+                {/*Left renderer */}
+                <div className="flex-1 xl:flex">
+                    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+                        <PdfRenderer url={file.url}/>
+                    </div>
+                </div>
+                {/*Chat and messages wrapper */}
+                <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+                    <ChatWrapper />
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Page
