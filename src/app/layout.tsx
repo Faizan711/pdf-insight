4fff1808fed48807ecafdb792c5cb3ae851b7c0d
Faizan <faizanalam980@gmail.com>
2023-12-03 11:47:42 +0530

    added simple bar css in import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Toaster } from "@/components/ui/toaster"
 import Providers from "@/components/Providers";
 
 import "react-loading-skeleton/dist/skeleton.css"
+import "simplebar-react/dist/simplebar.min.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
