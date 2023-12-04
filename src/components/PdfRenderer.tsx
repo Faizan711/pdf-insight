53744df725818ef8e033b18b91ae32c79b99ea52
Faizan <faizanalam980@gmail.com>
2023-12-04 19:16:38 +0530

    completed renderer component

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -26,6 +26,7 @@ import {
   DropdownMenuItem,
 } from "./ui/dropdown-menu";
 import SimpleBar from "simplebar-react";
+import PdfFullScreen from "./PdfFullScreen";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -40,6 +41,9 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
   const [currPage, setCurrPage] = useState<number>(1);
   const [scale, setScale] = useState<number>(1);
   const [rotation, setRotation] = useState<number>(0);
+
+  const[renderedScale, setRenderedScale] = useState<number | null>(null)
+  const isLoading = renderedScale !== scale
 
   const customPageValidator = z.object({
     page: z
@@ -76,6 +80,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             disabled={currPage <= 1}
             onClick={() => {
               setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+              setValue("page", String(currPage - 1));
             }}
             variant="ghost"
             aria-label="previous page"
@@ -108,6 +113,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               setCurrPage((prev) =>
                 prev + 1 > numPages! ? numPages! : prev + 1
               );
+              setValue("page", String(currPage + 1));
             }}
             variant="ghost"
             aria-label="next page"
@@ -140,12 +146,15 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
             </DropdownMenuContent>
           </DropdownMenu>
 
-          <Button 
-            onClick={() => setRotation((prev) => prev+90)} 
-            variant="ghost" 
-            aria-label="rotate 90 degrees">
+          <Button
+            onClick={() => setRotation((prev) => prev + 90)}
+            variant="ghost"
+            aria-label="rotate 90 degrees"
+          >
             <RotateCw className="h-4 w-4" />
           </Button>
+
+          <PdfFullScreen fileUrl={url} />
         </div>
       </div>
 
@@ -171,11 +180,27 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               file={url}
               className="max-h-full"
             >
-              <Page
+              {isLoading && renderedScale ? (<Page
                 width={width ? width : 1}
                 pageNumber={currPage}
                 scale={scale}
                 rotate={rotation}
+                key={"@" + renderedScale}
+              />) : null}
+
+              <Page
+                className={cn(isLoading ? "hidden" : "")}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={"@" + scale}
+                loading={
+                    <div className="flex justify-center">
+                      <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                    </div>
+                }
+                onRenderSuccess={() => setRenderedScale(scale)}
               />
             </Document>
           </div>
