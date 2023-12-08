5aa611e7e8e80e9fdad2a9ab19612f55971ab87c
Faizan <faizanalam980@gmail.com>
2023-12-08 16:03:51 +0530

    integrated pinecone and openai

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,21 +1,22 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
- 
-const f = createUploadthing();
- 
-export const ourFileRouter = {
- 
-  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
-   
-    .middleware(async ({ req }) => {
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { pinecone } from "@/lib/pinecone";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession();
       const user = await getUser();
 
-      if(!user || !user.id) throw new Error("unauthorized")
+      if (!user || !user.id) throw new Error("unauthorized");
 
-      return {userId: user.id };
+      return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const createdFile = await db.file.create({
@@ -25,9 +26,53 @@ export const ourFileRouter = {
           userId: metadata.userId,
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
           uploadStatus: "PROCESSING",
-        }
-      })
+        },
+      });
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        //vectorize and index entire document
+        // const pinecone = await getPineconeClient()
+        const pineconeIndex = pinecone.Index("pdf-insight");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
