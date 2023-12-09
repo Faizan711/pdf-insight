2947b408ded9a037f20655c777fa9126ebeaa5e9
Faizan <faizanalam980@gmail.com>
2023-12-09 19:47:16 +0530

    modified for new api

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,6 +3,7 @@ import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { z } from "zod";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -38,6 +39,55 @@ export const appRouter = router({
         userId,
       },
     });
+  }),
+
+  getFileMessages: privateProcedure.input(
+    z.object({
+      limit: z.number().min(1).max(100).nullish(),
+      cursor: z.string().nullish(),
+      fileId: z.string()
+    })
+  ).query(async ({ctx, input}) => {
+    const {userId} = ctx
+    const {fileId, cursor} = input
+    const limit = input.limit ?? INFINITE_QUERY_LIMIT
+
+    const file = await db.file.findFirst({
+      where: {
+        id: fileId,
+        userId
+      }
+    })
+
+    if(!file) throw new TRPCError({code: "NOT_FOUND"})
+
+    const messages = await db.message.findMany({
+      take: limit + 1,
+      where: {
+        fileId
+      },
+      orderBy: {
+        createdAt: 'desc'
+      },
+      cursor: cursor ? {id: cursor} : undefined,
+      select: {
+        id: true,
+        isUserMessage: true,
+        createdAt: true,
+        text: true
+      }
+    })
+
+    let nextCursor: typeof cursor | undefined = undefined
+    if(messages.length > limit){
+      const nextItem = messages.pop()
+      nextCursor = nextItem?.id
+    }
+
+    return {
+      messages,
+      nextCursor,
+    }
   }),
   getFileUploadStatus: privateProcedure
     .input(z.object({ fileId: z.string() }))
