19f5f5f9ff19b14431ef54989708ff0a3b94f399
Faizan <faizanalam980@gmail.com>
2023-12-10 19:45:34 +0530

    completed messages and loading more

--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -4,7 +4,8 @@ import { Loader2, MessageSquareIcon } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import Message from "./Message";
 import { ChatContext } from "./ChatContext";
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { useIntersection } from "@mantine/hooks";
 
 interface MessagesProps {
   fileId: string;
@@ -43,6 +44,19 @@ const Messages = ({ fileId }: MessagesProps) => {
     ...(messages ?? []),
   ];
 
+  const lastMessageRef = useRef<HTMLDivElement>(null)
+
+  const { ref, entry} = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1
+  })
+
+  useEffect(() => {
+    if(entry?.isIntersecting){
+      fetchNextPage()
+    }
+  },[entry, fetchNextPage])
+
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
@@ -54,6 +68,7 @@ const Messages = ({ fileId }: MessagesProps) => {
           if (i === combinedMessages.length - 1) {
             return (
               <Message
+                ref={ref}
                 message={message}
                 key={message.id}
                 isNextMessageSamePerson={isNextMessageSamePerson}
