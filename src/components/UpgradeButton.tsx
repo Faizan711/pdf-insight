38af38a6aa63f65e9da5884563337672ca7798f2
Faizan <faizanalam980@gmail.com>
2023-12-13 19:35:51 +0530

    upgradeButton component

---
+++ src/components/UpgradeButton.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { ArrowRight } from "lucide-react";
+import { Button } from "./ui/button";
+import { trpc } from "@/app/_trpc/client";
+
+const UpgradeButton = () => {
+
+  const { mutate: createStripeSession} = trpc.createStripeSession.useMutation({
+    onSuccess: ({url}) => {
+        window.location.href = url ?? "/dashboard/billing"
+    }
+  })
+
+  return (
+    <Button className="w-full" onClick={() => createStripeSession()}>
+      Upgrade Now <ArrowRight className="h-5 w-5 ml-1.5" />
+    </Button>
+  )
+}
+
+export default UpgradeButton;
