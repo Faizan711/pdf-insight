cd9b391a7c278f0db33d2a9456233a64342ed8f6
Faizan <faizanalam980@gmail.com>
2023-12-13 20:12:17 +0530

    created middleware with kinde

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
+
+
+export const config  = {
+    matcher: ["/dashboard/:path*", "/auth-callback"]
+}
+
+export default authMiddleware 
