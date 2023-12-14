aa64fbc9bba327243bc88b22e1120b8d48036c39
Faizan <faizanalam980@gmail.com>
2023-12-14 16:55:57 +0530

    removed middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
 
-
 export const config  = {
-    matcher: ['/dashboard/:path*', '/auth-callback']
+    // matcher: ['/dashboard/:path*', '/auth-callback']
+    matcher: ['/da', '/aut']
 }
 
 export default authMiddleware 
