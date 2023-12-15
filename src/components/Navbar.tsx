400bf632243d5b9c60af580c1326d1fc0cb438b7
Faizan <faizanalam980@gmail.com>
2023-12-15 17:37:06 +0530

    added logged in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,49 +1,104 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight, Github, GithubIcon } from "lucide-react";
+import UserAccountNav from "./UserAccountNav";
 
-const Navbar = () => {
-    return (
-        
-        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
-            <MaxWidthWrapper>
-                <div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
-                    <Link href='/' className="flex z-40 font-semibold">
-                        <span className="">Pdf-insight</span>
-                    </Link>
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-                    {/* TODO: add mobile navbar */}
+  return (
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200 ">
+          <Link href="/" className="flex z-40 font-semibold">
+            <span className="">Pdf-insight</span>
+          </Link>
 
-                    <div className="hidden items-center space-x-4 sm:flex ">
-                        <>
-                            <Link target="_blank" href='https://github.com/Faizan711/pdf-insight' className={buttonVariants({
-                                variant:"outline",
-                                size: 'sm',
-                            })}><Github className="h-4 w-4"/></Link>
-                            <Link href='/pricing' className={buttonVariants({
-                                variant:"ghost",
-                                size: 'sm',
-                            })}>Pricing</Link>
-                            <LoginLink className={buttonVariants({
-                                variant:"ghost",
-                                size: 'sm',
-                            })}>
-                                Sign In
-                            </LoginLink>
-                            <RegisterLink className={buttonVariants({
-                                
-                                size: 'sm',
-                            })}>
-                                Get Started <ArrowRight className="ml-1.5 h-5 w-5"/>
-                            </RegisterLink>
-                        </>
-                    </div>
-                </div>
-            </MaxWidthWrapper>
-        </nav>
-    )
-}
+          {/* TODO: add mobile navbar */}
+
+          <div className="hidden items-center space-x-4 sm:flex ">
+            {!user ? (
+              <>
+                <Link
+                  target="_blank"
+                  href="https://github.com/Faizan711/pdf-insight"
+                  className={buttonVariants({
+                    variant: "outline",
+                    size: "sm",
+                  })}
+                >
+                  <Github className="h-4 w-4" />
+                </Link>
+                <Link
+                  href="/pricing"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <LoginLink
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Sign In
+                </LoginLink>
+                <RegisterLink
+                  className={buttonVariants({
+                    size: "sm",
+                  })}
+                >
+                  Get Started <ArrowRight className="ml-1.5 h-5 w-5" />
+                </RegisterLink>
+              </>
+            ) : (
+              <>
+                <Link
+                  target="_blank"
+                  href="https://github.com/Faizan711/pdf-insight"
+                  className={buttonVariants({
+                    variant: "outline",
+                    size: "sm",
+                  })}
+                >
+                  <Github className="h-4 w-4" />
+                </Link>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  Dashboard
+                </Link>
+
+                <UserAccountNav
+                  name={
+                    !user.given_name || !user.family_name
+                      ? "Your Account"
+                      : `${user.given_name} ${user.family_name}`
+                  }
+                  email={user.email ?? ""}
+                  imageUrl={user.picture ?? ""}
+                />
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
 
 export default Navbar;
