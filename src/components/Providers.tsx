a4c2452e7fd61f5f8ad5d95c1c5b8c7d7b6e99e3
Faizan <faizanalam980@gmail.com>
2023-12-15 17:39:15 +0530

    added absoluteURL in httpbatchLink

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
+import { absoluteUrl } from "@/lib/utils";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -11,7 +12,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: absoluteUrl("/api/trpc"),
         }),
       ],
     })
