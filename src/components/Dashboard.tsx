0f7131187e58f3983fc174caa954332047d30f5b
Faizan <faizanalam980@gmail.com>
2023-12-15 18:59:55 +0530

    padding given

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -30,7 +30,7 @@ const Dashboard = () => {
   });
 
   return (
-    <main className="mx-auto max-w-7xl md:p-10">
+    <main className="mx-auto max-w-7xl px-5 md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
 
