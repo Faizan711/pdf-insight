701dfb260a423ac5fec643b887066b3a70bf67ff
Faizan <faizanalam980@gmail.com>
2023-12-16 13:00:37 +0530

    added upload page count restrictions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,48 @@ export function absoluteUrl(path: string){
     return `https://${process.env.VERCEL_URL}${path}`
   }
   return `http://localhost:${process.env.PORT ?? 3000}${path}`
+}
+
+export function constructMetadata({
+  title= "Pdf-insight - the Saas for documents",
+  description= "Pdf-insight is an open-source software to help you chat with your PDFs.",
+  image= "/thumbnail.png",
+  icons= "/favicon.ico",
+  noIndex= false
+}:{
+  title?: string,
+  description?: string,
+  image?: string,
+  icons?:string,
+  noIndex?:boolean
+} = {}) : Metadata {
+  return{
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images:[
+        {
+          url: image
+        }
+      ]
+    },
+    twitter:{
+      card: "summary_large_image",
+      title,
+      description,
+      images:[image],
+      creator: "@MdFaizan711"
+    },
+    icons,
+    metadataBase:new URL('https://pdf-insight-fa.vercel.app'),
+    themeColor: "#fff",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
 }
